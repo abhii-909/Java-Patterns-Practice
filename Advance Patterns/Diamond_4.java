bb7d3a5e5a56b073fb2a48030099ec9337c1ce36
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2024-07-27 12:14:09 +0530

    Update Diamond_4.java

--- Advance Patterns/Diamond_4.java
+++ Advance Patterns/Diamond_4.java
@@ -11,8 +11,8 @@ public class Diamond_4 {
             }
 
             // stars
-            for(int j = 1; j <= i; j++){
-                System.out.print("*" + " ");
+            for(int j = 1; j <= (2*i)-1; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -26,8 +26,8 @@ public class Diamond_4 {
             }
 
             // stars
-            for(int j = 1; j <= i; j++){
-                System.out.print("*" + " ");
+            for(int j = 1; j <= (2*i)-1; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
