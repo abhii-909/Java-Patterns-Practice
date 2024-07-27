bb06b67c3b2f67a1e9b7bfc9c5342169568691fc
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2024-07-27 11:38:54 +0530

    Update Pyramid8.java

--- Basic Patterns/Pyramid8.java
+++ Basic Patterns/Pyramid8.java
@@ -9,7 +9,7 @@ public class Pyramid8 {
             }
 
             //inner loop 2 -> star print
-            for( int j = 1; j <= i; j++){
+            for( int j = 1; j <= (2*i) - 1; j++){
                 System.out.print("*" + " ");
             }
             System.out.println();
