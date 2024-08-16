570081c31d77db1cfd92caaee142206ee20f11d0
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2024-08-16 21:52:17 +0530

    added new pattern

---
+++ Advance Patterns/Alphabet_triangle.java
@@ -0,0 +1,14 @@
+public class Alphabet_triangle {
+    public static void main(String[] args) {
+        nLetterTriangle(4);
+    }
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+        for (int i = 1; i <= n; i++) {
+            for (char ch = 'A'; ch < 'A'+ i ; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
